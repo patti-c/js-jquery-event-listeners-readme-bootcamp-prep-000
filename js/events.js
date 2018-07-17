594b4edaf944b47f22076f484d616a074cd61516
Patricia Carter <patricia4194@gmail.com>
2018-07-17 20:04:18 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,40 @@
 //define functions here
 
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!');
+    return;
+  });
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+    return;
+  });
+}
+
+function pressIt() {
+  $('#typing').on('keydown', function(key) {
+    if(key.which === 71) {
+      alert('You did the thing!');
+    }
+  });
+}
+
+function submitIt() {
+  $('form').on('submit', function() {
+    alert('Your form is going to be submitted now.')
+  })
+}
+
+
+frameIt();
+
 $(document).ready(function(){
 
-// call functions here
+getIt();
+pressIt();
+submitIt()
 
 });
